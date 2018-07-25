9bf5274449cdc5210c205979dada1aea51f11787
Marissa Zhong <mwz2101@columbia.edu>
2018-07-25 13:00:56 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -9,7 +9,7 @@ class StudentsController < ApplicationController
 	end
 
 	def create
-	  @student = Student.new
+	  @student = Student.new(student_params(:first_name, :last_name))
 	  @student.first_name = params[:first_name]
 	  @student.last_name = params[:last_name]
 	  @student.save
@@ -22,7 +22,7 @@ class StudentsController < ApplicationController
 
 	def update
 	  @student = Student.find(params[:id])
-		@student.update(params.require(:student))
+		@student.update(student_params(:first_name, :last_name))
     redirect_to student_path(@student)
 	end
 
