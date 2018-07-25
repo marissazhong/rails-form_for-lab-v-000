8c20a4812a7ec2e624efab127705308b9bd13c68
Marissa Zhong <mwz2101@columbia.edu>
2018-07-25 12:55:58 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -10,8 +10,8 @@ class StudentsController < ApplicationController
 
 	def create
 	  @student = Student.new
-	  @student.title = params[:title]
-	  @student.room_number = params[:room_number]
+	  @student.first_name = params[:first_name]
+	  @student.last_name = params[:last_name]
 	  @student.save
 	  redirect_to student_path(@student)
 	end
