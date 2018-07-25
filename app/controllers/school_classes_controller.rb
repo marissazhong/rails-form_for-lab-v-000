7a2ac72108a46d60eb97d507b69a6824a43e14c9
Marissa Zhong <mwz2101@columbia.edu>
2018-07-25 13:00:03 +0000

    automatically backed up by learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -9,9 +9,7 @@ class SchoolClassesController < ApplicationController
 	end
 
 	def create
-	  @school_class = SchoolClass.new
-	  @school_class.title = params[:title]
-	  @school_class.room_number = params[:room_number]
+	  @school_class = SchoolClass.new(school_class_params(:title, :room_number))
 	  @school_class.save
 	  redirect_to school_class_path(@school_class)
 	end
@@ -22,11 +20,11 @@ class SchoolClassesController < ApplicationController
 
 	def update
 	  @school_class = SchoolClass.find(params[:id])
-		@school_class.update(params.require(:school_class))
+		@school_class.update(school_class_params(:title, :room_number))
     redirect_to school_class_path(@school_class)
 	end
 
-	def post_params(*args)
+	def school_class_params(*args)
 		params.require(:school_class).permit(*args)
 	end
 end
